9bd33d5d70880e7485ecd8bce83eb82afcce4c24
Mirjam van Mourik <mirja@MIMSSURFACE>
2024-12-01 21:17:31 +0100

    move method to console helper

---
+++ AdventOfCode2024/Helpers/ConsoleHelper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace AdventOfCode2024.Helpers
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteFestive(string input)
+        {
+            string text = $"{input}";
+            bool useGreen = true; // Start with green for the first non-space character
+
+            foreach (char c in text)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    // Write whitespace without changing the color or toggling
+                    Console.Write(c);
+                }
+                else
+                {
+                    // Alternate colors
+                    Console.ForegroundColor = useGreen ? ConsoleColor.Green : ConsoleColor.Red;
+                    Console.Write(c);
+
+                    // Toggle color for the next non-space character
+                    useGreen = !useGreen;
+                }
+            }
+
+            // Reset console color to default
+            Console.ResetColor();
+
+            // Add a newline for better formatting
+            Console.WriteLine();
+        }
+
+        public static void AddChristmasTree()
+        {
+            // Set up colors
+            ConsoleColor treeColor = ConsoleColor.DarkGreen;
+            ConsoleColor redOrnament = ConsoleColor.Red;
+            ConsoleColor yellowOrnament = ConsoleColor.Yellow;
+            ConsoleColor logColor = ConsoleColor.White;
+
+            // Define the tree rows
+            string[] tree = {
+            "        *",
+            "       /|\\",
+            "      /*|O\\",
+            "     /*/|\\*\\",
+            "    /X/O|*\\X\\",
+            "   /*/X/|\\O\\*\\",
+            "  /O/*/X|*\\X\\O\\",
+            " /X/O/*/|\\O\\X\\*\\",
+            "/X/O/X/|O|X\\*O\\X\\",
+            "       |||",
+            "       |||",
+            "       |||"
+        };
+
+            // Output each row with appropriate colors
+            foreach (string line in tree)
+            {
+                if (line.Contains("|||"))
+                {
+                    Console.ForegroundColor = logColor;
+                    Console.Write(line);
+                    Console.WriteLine();
+                    continue;
+                }
+
+                foreach (char c in line)
+                {
+                    // Set color based on the character
+                    if (c == 'X' || c == '/' || c == '|' || c == '\\')
+                    {
+                        Console.ForegroundColor = treeColor;
+                    }
+                    else if (c == 'O')
+                    {
+                        Console.ForegroundColor = redOrnament;
+                    }
+                    else if (c == '*')
+                    {
+                        Console.ForegroundColor = yellowOrnament;
+                    }
+                    else
+                    {
+                        Console.ResetColor();
+                    }
+
+                    // Write character
+                    Console.Write(c);
+                }
+
+                // Move to the next line
+                Console.WriteLine();
+            }
+
+            // Reset color after finishing
+            Console.ResetColor();
+        }
+    }
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode2024.Days;
-using AdventOfCode2024.Extensions;
+using AdventOfCode2024.Helpers;
 using System;
 using System.Diagnostics;
 
 Console.ForegroundColor = ConsoleColor.DarkRed;
 ConsoleHelper.WriteFestive("Advent of Code 2024");
-AddChristmasTree();
+ConsoleHelper.AddChristmasTree();
 Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine("by Mirjam van Mourik\n");
 
@@ -34,71 +34,4 @@ static void GetAnswersAndPrintToConsole(IDay day)
     Console.WriteLine($"Result 2: {result2}");
     Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds} ms");
     Console.WriteLine();
-}
-
-static void AddChristmasTree()
-{
-    // Set up colors
-    ConsoleColor treeColor = ConsoleColor.DarkGreen;
-    ConsoleColor redOrnament = ConsoleColor.Red;
-    ConsoleColor yellowOrnament = ConsoleColor.Yellow;
-    ConsoleColor logColor = ConsoleColor.White;
-
-    // Define the tree rows
-    string[] tree = {
-            "        *",
-            "       /|\\",
-            "      /*|O\\",
-            "     /*/|\\*\\",
-            "    /X/O|*\\X\\",
-            "   /*/X/|\\O\\*\\",
-            "  /O/*/X|*\\X\\O\\",
-            " /X/O/*/|\\O\\X\\*\\",
-            "/X/O/X/|O|X\\*O\\X\\",
-            "       |||",
-            "       |||",
-            "       |||"
-        };
-
-    // Output each row with appropriate colors
-    foreach (string line in tree)
-    {
-        if (line.Contains("|||"))
-        {
-            Console.ForegroundColor = logColor;
-            Console.Write(line);
-            Console.WriteLine();
-            continue;
-        }
-
-        foreach (char c in line)
-        {
-            // Set color based on the character
-            if (c == 'X' || c == '/' || c == '|' || c == '\\')
-            {
-                Console.ForegroundColor = treeColor;
-            }
-            else if (c == 'O')
-            {
-                Console.ForegroundColor = redOrnament;
-            }
-            else if (c == '*')
-            {
-                Console.ForegroundColor = yellowOrnament;
-            }
-            else
-            {
-                Console.ResetColor();
-            }
-
-            // Write character
-            Console.Write(c);
-        }
-
-        // Move to the next line
-        Console.WriteLine();
-    }
-
-    // Reset color after finishing
-    Console.ResetColor();
 }
