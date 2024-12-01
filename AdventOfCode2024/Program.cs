b2ebdfc1fef08f0b0daacbfc116da4697ba1120e
Mirjam van Mourik <mirja@MIMSSURFACE>
2024-12-01 20:30:34 +0100

    Add stuff

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode2024.Days;
+using System;
 using System.Diagnostics;
 
 Console.ForegroundColor = ConsoleColor.DarkRed;
-Console.WriteLine("Advent of Code 2024\n");
+Console.WriteLine("Advent of Code 2024");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("        *\r");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("       /|\\\r\n      /*|O\\\r\n     /*/|\\*\\\r\n    /X/O|*\\X\\\r\n   /*/X/|\\O\\*\\\r\n  /O/*/X|*\\XO\\\r\n /X/O/*/|\\O\\X*\\\r\n/__|__|_|__|__\\\r\n      |||\r\n      |||\r\n      |||");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("by Mirjam van Mourik\n");
 
 #region Day1
 // Day 1
