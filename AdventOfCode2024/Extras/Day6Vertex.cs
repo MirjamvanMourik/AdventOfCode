40c03a1274292aea1bb01ae214fb71c45f3e1ce1
Mirjam van Mourik <mirja@MIMSSURFACE>
2024-12-07 15:04:53 +0100

    fix

--- AdventOfCode2024/Extras/Day6Vertex.cs
+++ AdventOfCode2024/Extras/Day6Vertex.cs
@@ -85,7 +85,6 @@
 
         public void Restore()
         {
-            // Do the reverse
             IsStartingPosition = SavedIsStartingPosition;
             IsObstacle = SavedIsObstacle;
             Visited = SavedVisited;
