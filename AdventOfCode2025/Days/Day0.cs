ba145f14fa088a91e3bfcef028e4d1df647d8444
Mirjam van Mourik <mirja@MIMSSURFACE>
2025-12-07 16:23:21 +0100

    Added base day

---
+++ AdventOfCode2025/Days/Day0.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode2025.Input;
+using ConsoleHelper;
+
+namespace AdventOfCode2025.Days
+{
+    public class Day0 : IDay
+    {
+        long IDay.Day => 0;
+        string IDay.Title => "Base";
+
+        public long GetFirstAnswer()
+        {
+            var banks = InputSplitter.SplitLinesToInt(Day0Base.Example);
+
+            var total = 0L;
+
+            return 0;
+        }
+
+        public long GetSecondAnswer()
+        {
+            var banks = InputSplitter.SplitLinesToInt(Day3JoltageRatings.Example);
+
+            var total = 0L;
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2025/Days/Day4.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode2025.Input;
+using ConsoleHelper;
+
+namespace AdventOfCode2025.Days
+{
+    public class Day4 : IDay
+    {
+        long IDay.Day => 4;
+        string IDay.Title => "Lobby";
+
+        public long GetFirstAnswer()
+        {
+            var banks = InputSplitter.SplitLinesToInt(Day3JoltageRatings.Input);
+
+            var total = 0L;
+
+            return 0;
+        }
+
+        public long GetSecondAnswer()
+        {
+            var banks = InputSplitter.SplitLinesToInt(Day3JoltageRatings.Input);
+
+            var total = 0L;
+
+            return 0;
+        }
+    }
+}
